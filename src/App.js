687916107a6a6bf75faa793cd4e8d54bcfd8cab6
Aleksi <a12345@live.fi>
2020-11-23 15:34:21 +0200

    Fetched todo data from api
    - Used React Hooks useState and useEffect
    - Used Axios library to get todos
    - List todos in Bootstrap Cards using React Bootstrap

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import TopBar from "./components/TopBar";
+import TodoList from './components/TodoList';
 
 function App() {
   return (
     <div className="App">
       <TopBar />
+      <TodoList />
     </div>
   );
 }
--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,9 +1,29 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+import { Card } from 'react-bootstrap';
 
 export default function TodoList() {
+  const [todos, setTodos] = useState([]);
+  
+  useEffect(() => {
+    axios.get('http://localhost:8080/api/all')
+  .then(function (response) {
+    setTodos(response.data);
+    console.log(todos);
+  })
+  .catch(function (error) {
+    console.log(error);
+  })
+  }, []);
+
+  const listTodos = todos.map((todo) =>
+    <Card>
+      <Card.Body key={todo.id}>{todo.todo}</Card.Body>
+    </Card>
+  );
   return (
     <div>
-      
+        {listTodos}
     </div>
   )
 }
